c2544ae1aa01a026009865a25857c6abf5172910
Micah Wagner <micahwagner@Micahs-MacBook-Pro.local>
2023-10-07 14:40:37 -0700

    added NFA structure and stuff

--- RE_TO_NFA.py
+++ RE_TO_NFA.py
@@ -1,10 +1,49 @@
 import sys
-input_file = open(sys.argv[1], 'r')
 
+stack = []
+
+class NFA:
+	def __init__(self, start, accept, transition):
+		self.start = start
+		self.accept = accept
+		self.transition = transition
+
+	@staticmethod
+	def concat_NFAs(NFA1, NFA2):
+		pass
+
+	@staticmethod
+	def union_NFAs(NFA1, NFA2):
+		pass
+
+	@staticmethod
+	def kleene_star(NFA1):
+		pass
+
+input_file = open(sys.argv[1], 'r')
+alphabet = ['a', 'b', 'c', 'd', 'e']
+operations = ['|', '&', '*']
 
 Lines = input_file.readlines()
  
 count = 0
-# Strips the newline character
+
 for line in Lines:
-    print(line.strip())
+    for i, v in enumerate(line):
+    	if(v != '\n'):
+    		if(v == '&'):
+    			NFA2 = stack.pop()
+    			NFA1 = stack.pop()
+    			stack.append(NFA.concat_NFAs(NFA1, NFA2))
+    		elif(v == '|'):
+    			NFA2 = stack.pop()
+    			NFA1 = stack.pop()
+    			stack.append(NFA.union_NFAs(NFA1, NFA2))
+    		elif(v == '*'):
+    			NFA1 = stack.pop()
+    			stack.append(NFA.kleene_star(NFA1))
+    		else:
+    			stack.append(NFA('q' + str(1), 'q' + str(2), ['(q1,' + str(v) + ')->q2']))
+    	else:
+    		break
+    print('\n')
